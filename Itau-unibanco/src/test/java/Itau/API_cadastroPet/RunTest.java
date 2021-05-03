f9ada77dc4d2f4ef9d3dbe8047dc0ee3bf6e7f2a
dovd <dovd@BRPC014520.gft.com>
2021-05-03 16:54:48 -0300

    Estruturação de FrameWork

---
+++ Itau-unibanco/src/test/java/Itau/API_cadastroPet/RunTest.java
@@ -0,0 +1,13 @@
+package Itau.API_cadastroPet;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
+
+public class RunTest {
+
+}
